477d0a5e9f838eba9dd1ee16447c37afe7141a7e
Sagar Dolui <sagar03d@gmail.com>
2021-06-30 17:45:47 +0530

    Update FormSubmitter.js

--- FormSubmitter.js
+++ FormSubmitter.js
@@ -1,58 +1,54 @@
-var SubmitForm = function(){
-        $(".savebtn").html('Please Wait...');
-        $(".savebtn").attr('disabled', true);
-        $('.errormessage').html('');
-        var formData = new FormData(this);
-        $.ajax({
-            headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-            method      : 'POST',
-            data        : formData,
-            url         : $(this).attr('action'),
-            processData : false, // Don't process the files
-            contentType : false, // Set content type to false as jQuery will tell the server its a query string request
-            dataType    : 'json',
-            success     : function(response){
-                if(response.success == true)
-                {
-                    swal.fire({   
-                            title: "Success",   
-                            text: response.message,   
-                            icon: "success",   
-                            showCancelButton: false,
-                            showConfirmButton: false,
-                            timer: 2000
-                    });
-                    let redirect = arguments[0];
-                    if(redirect)
+$('form').each(function(){
+       let formid = $(this).closest("form").attr('id');
+       let redirect = $(this).closest("form").attr('data-redirect');
+       $("form#"+formid).on('submit', function(e){
+            $(".savebtn").html('Please Wait...');
+            $(".savebtn").attr('disabled', true);
+            $('.errormessage').html('');
+            var formData = new FormData(this);
+            $.ajax({
+                headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+                method      : 'POST',
+                data        : formData,
+                url         : $(this).attr('action'),
+                processData : false, // Don't process the files
+                contentType : false, // Set content type to false as jQuery will tell the server its a query string request
+                dataType    : 'json',
+                success     : function(response){
+                    if(response.success == true)
                     {
+                        swal.fire({   
+                                title: "Success",   
+                                text: response.message,   
+                                icon: "success",   
+                                showCancelButton: false,
+                                showConfirmButton: false,
+                                timer: 2000
+                        });
                         window.location.href=redirect;
                     }
                     else
                     {
-                        window.location.reload();
+                        $.notify(""+response.data+"", {type:"danger"});
+                        $(".savebtn").html('Save');
+                        $(".savebtn").attr('disabled', false);
                     }
-                }
-                else
-                {
-                    $.notify(""+response.data+"", {type:"danger"});
+                },
+                error       : function(data){
+                    var errors = $.parseJSON(data.responseText);
+                    $.each(errors.errors, function(index, value) {
+                        $('[name="'+index+'"]').after('<span class="text-danger errormessage">'+value+'</span>');
+                    });
                     $(".savebtn").html('Save');
                     $(".savebtn").attr('disabled', false);
                 }
-            },
-            error       : function(data){
-                var errors = $.parseJSON(data.responseText);
-                $.each(errors.errors, function(index, value) {
-                    $('[name="'+index+'"]').after('<span class="text-danger errormessage">'+value+'</span>');
-                });
-                $(".savebtn").html('Save');
-                $(".savebtn").attr('disabled', false);
-            }
-
+    
+         });
+            return false;
         });
-        return false;
-    }
-
-    var DeleteRecord = function(){
+    });
+    
+var DeleteRecord = function(){
         var action = $(this).data('action');
             
         swal.fire({   
