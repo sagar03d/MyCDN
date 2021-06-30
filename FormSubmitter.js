e5c3ef64bc20dc4897849808136a0c9623cb3387
Sagar Dolui <sagar03d@gmail.com>
2021-06-30 16:36:36 +0530

    Added delete functionality

--- FormSubmitter.js
+++ FormSubmitter.js
@@ -43,3 +43,52 @@ var SubmitForm = function(){
         });
         return false;
     }
+
+    var DeleteRecord = function(){
+        var action = $(this).data('action');
+        Swal.fire({   
+            title: "Do you really want to delete ?",   
+            text: "",   
+            icon: "warning",   
+            showCancelButton: true,
+            showConfirmButton: true, 
+            confirmButtonColor: "#DD6B55",   
+            confirmButtonText: "Yes, Delete it!",
+            confirmButtonColor: "#FF0000"
+        });
+        $(".swal2-confirm").unbind().on('click', function(){
+            $.ajax({
+                headers   :{'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')},
+                method      : 'delete',
+                url         : action,
+                //processData : false, // Don't process the files
+                //contentType : false, // Set content type to false as jQuery will tell the server its a query string request
+                dataType    : 'json',
+                success     : function(response){
+                    if(response.success == true)
+                    {
+                        Swal.fire({   
+                                title: "Success",   
+                                text: response.message,   
+                                icon: "success",   
+                                showCancelButton: false,
+                                showConfirmButton: false,
+                                timer: 1000
+                        });
+                        window.location.href="";
+                        
+                    }
+                    else
+                    {
+                        $.notify(""+response.data+"", {type:"danger"});
+                    }
+                },
+                error       : function(data){
+                    var errors = $.parseJSON(data.responseText);
+                    $.each(errors, function(index, value) {
+                        $.notify(""+value+"", {type:"danger"});
+                    });
+                }
+            });
+        });
+    }
