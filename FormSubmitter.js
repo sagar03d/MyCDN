45a3b370db39fe7c91ef4627465ef689777a9794
Sagar Dolui <sagar03d@gmail.com>
2021-10-21 00:53:57 +0530

    1.1.0

--- FormSubmitter.js
+++ FormSubmitter.js
@@ -1,77 +1,81 @@
 $('form').each(function(){
-       let formid = $(this).closest("form").attr('id');
-       let ignore = $(this).closest("form").data('ignore');
-       if(!ignore){
-       $("form#"+formid).on('submit', function(e){
-            let btnval = $(".savebtn").text();
-            let redirect = $(this).attr('data-redirect');
-            $(".savebtn").html('Please Wait...');
-            $(".savebtn").attr('disabled', true);
-            $('.errormessage').html('');
-            
-            if( typeof(CKEDITOR) !== "undefined" )
-            {
-                for ( instance in CKEDITOR.instances )
-                    CKEDITOR.instances[instance].updateElement();
-            }
-
-            var formData = new FormData(this);
-            $.ajax({
-                headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-                method      : 'POST',
-                data        : formData,
-                url         : $(this).attr('action'),
-                processData : false, // Don't process the files
-                contentType : false, // Set content type to false as jQuery will tell the server its a query string request
-                dataType    : 'json',
-                success     : function(response){
-                    if(response.success == true)
-                    {
-                        swal.fire({   
-                                title: "Success",   
-                                text: response.message,   
-                                icon: "success",   
-                                showCancelButton: false,
-                                showConfirmButton: false,
-                                timer: 2000
-                        });
-                        
-                        if(typeof response.redirect !== 'undefined'){
-                            window.location.href=response.redirect;
-                        }
-                        else{
-                            window.location.href=redirect;
-                        }
-                    }
-                    else
-                    {
-                        $.notify(""+response.data+"", {type:"danger"});
-                        $(".savebtn").html(btnval);
-                        $(".savebtn").attr('disabled', false);
-                    }
-                },
-                error       : function(data){
-                    var errors = $.parseJSON(data.responseText);
-                    $.each(errors.errors, function(index, value) {
-                        if($('[name="'+index+'"]').length)
-                        {
-                            $('[name="'+index+'"]').after('<span class="text-danger errormessage">'+value+'</span>');
-                        }
-                        else
-                        {
-                           $('[name="'+index+'[]"]').after('<span class="text-danger errormessage">'+value+'</span>');
-                        }
-                    });
-                    $(".savebtn").html(btnval);
-                    $(".savebtn").attr('disabled', false);
-                }
-    
+    let formid = $(this).closest("form").attr('id');
+    let ignore = $(this).closest("form").data('ignore');
+    if(!ignore){
+    $("form#"+formid).on('submit', function(e){
+         let btnval = $(".savebtn").text();
+         let redirect = $(this).attr('data-redirect');
+         $(".savebtn").html('Please Wait...');
+         $(".savebtn").attr('disabled', true);
+         $('.errormessage').html('');
+         $('.error').each(function(){
+             $(this).removeClass('error');
          });
-            return false;
-        });
-       }
-    });
-    
+         
+         if( typeof(CKEDITOR) !== "undefined" )
+         {
+             for ( instance in CKEDITOR.instances )
+                 CKEDITOR.instances[instance].updateElement();
+         }
+
+         var formData = new FormData(this);
+         $.ajax({
+             headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+             method      : 'POST',
+             data        : formData,
+             url         : $(this).attr('action'),
+             processData : false, // Don't process the files
+             contentType : false, // Set content type to false as jQuery will tell the server its a query string request
+             dataType    : 'json',
+             success     : function(response){
+                 if(response.success == true)
+                 {
+                     swal.fire({   
+                             title: "Success",   
+                             text: response.message,   
+                             icon: "success",   
+                             showCancelButton: false,
+                             showConfirmButton: false,
+                             timer: 2000
+                     });
+                     
+                     if(typeof response.redirect !== 'undefined'){
+                         window.location.href=response.redirect;
+                     }
+                     else{
+                         window.location.href=redirect;
+                     }
+                 }
+                 else
+                 {
+                     $.notify(""+response.data+"", {type:"danger"});
+                     $(".savebtn").html(btnval);
+                     $(".savebtn").attr('disabled', false);
+                 }
+             },
+             error       : function(data){
+                 var errors = $.parseJSON(data.responseText);
+                 $.each(errors.errors, function(index, value) {
+                     $('[name="'+index+'"]').addClass('error');
+                     if($('[name="'+index+'"]').length)
+                     {
+                         $('[name="'+index+'"]').after('<span class="text-danger errormessage">'+value+'</span>');
+                     }
+                     else
+                     {
+                        $('[name="'+index+'[]"]').after('<span class="text-danger errormessage">'+value+'</span>');
+                     }
+                 });
+                 $(".savebtn").html(btnval);
+                 $(".savebtn").attr('disabled', false);
+             }
+ 
+      });
+         return false;
+     });
+    }
+ });
+     
 var DeleteRecord = function(){
         var action = $(this).data('action');
             
