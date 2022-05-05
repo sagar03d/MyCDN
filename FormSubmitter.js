47c122019ea38e454e312b2184b5616e9249cc33
Sagar Dolui <sagar03d@gmail.com>
2022-05-05 19:29:34 +0530

    fixeed removed is-invalid on valid

--- FormSubmitter.js
+++ FormSubmitter.js
@@ -18,7 +18,7 @@ $('form').each(function(){
          $(".savebtn").attr('disabled', true);
          $('.errormessage').html('');
          $('.error').each(function(){
-             $(this).removeClass('error');
+             $(this).removeClass('error is-invalid');
          });
          
          if( typeof(CKEDITOR) !== "undefined" )
