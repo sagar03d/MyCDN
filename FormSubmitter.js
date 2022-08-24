7b769007884866850d322bd18f6257fe325a97d8
Sagar Dolui <sagar03d@gmail.com>
2022-08-24 16:13:01 +0530

    Update FormSubmitter.js

--- FormSubmitter.js
+++ FormSubmitter.js
@@ -10,90 +10,95 @@ document.head.appendChild(jQueryScript);
 $('form').each(function(){
     let formid = $(this).closest("form").attr('id');
     let ignore = $(this).closest("form").data('ignore');
-    if(!ignore){
-    $("form#"+formid).on('submit', function(e){
-         let btnval = $(".savebtn").text();
-         let redirect = $(this).attr('data-redirect');
-         $(".savebtn").html('Please Wait...');
-         $(".savebtn").attr('disabled', true);
-         $('.errormessage').html('');
-         $('.error').each(function(){
-             $(this).removeClass('error');
-             $(this).removeClass('is-invalid');
-         });
-         
-         if( typeof(CKEDITOR) !== "undefined" )
-         {
-             for ( instance in CKEDITOR.instances )
-                 CKEDITOR.instances[instance].updateElement();
-         }
+    if(!ignore) {
+        $("form#"+formid).on('submit', function(e){
+            
+            let btnval = $(this).find('.savebtn').text();
+            var btnvalobj = $(this).find('.savebtn');
+            let redirect = $(this).attr('data-redirect');
+            btnvalobj.html('Please Wait...');
+            btnvalobj.attr('disabled', true);
 
-         var formData = new FormData(this);
-         $.ajax({
-             headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-             method      : 'POST',
-             data        : formData,
-             url         : $(this).attr('action'),
-             processData : false, // Don't process the files
-             contentType : false, // Set content type to false as jQuery will tell the server its a query string request
-             dataType    : 'json',
-             success     : function(response){
-                 if(response.success == true)
-                 {
-                     swal.fire({   
-                             title: "Success",   
-                             text: response.message,   
-                             icon: "success",   
-                             showCancelButton: false,
-                             showConfirmButton: false,
-                             timer: 1500
-                     });
-                     
-                     if(typeof response.redirect !== 'undefined'){
-                        setTimeout(() => {
-                            window.location.href=response.redirect;
-                         }, 1000);
-                     }
-                     else{
-                        setTimeout(() => {
-                            window.location.href=redirect;
-                         }, 1000);
-                     }
-                 }
-                 else
-                 {
-                    swal.fire({   
-                        title: "Error",   
-                        text: response.message,   
-                        icon: "error", 
-                        showCancelButton: false,
-                        showConfirmButton: false,
-                        timer: 1500
+            $('.errormessage').html('');
+            $('.error').each(function(){
+                $(this).removeClass('error');
+                $(this).removeClass('is-invalid');
+            });
+            
+            if( typeof(CKEDITOR) !== "undefined" )
+            {
+                for ( instance in CKEDITOR.instances )
+                    CKEDITOR.instances[instance].updateElement();
+            }
+
+            var formData = new FormData(this);
+            $.ajax({
+                headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+                method      : 'POST',
+                data        : formData,
+                url         : $(this).attr('action'),
+                processData : false, // Don't process the files
+                contentType : false, // Set content type to false as jQuery will tell the server its a query string request
+                dataType    : 'json',
+                success     : function(response){
+                    if(response.success == true)
+                    {
+                        swal.fire({   
+                                title: "Success",   
+                                text: response.message,   
+                                icon: "success",   
+                                showCancelButton: false,
+                                showConfirmButton: false,
+                                timer: 1500
+                        });
+                        
+                        if(typeof response.redirect !== 'undefined'){
+                            setTimeout(() => {
+                                window.location.href=response.redirect;
+                            }, 1000);
+                        }
+                        else{
+                            setTimeout(() => {
+                                window.location.href=redirect;
+                            }, 1000);
+                        }
+                    }
+                    else
+                    {
+                        swal.fire({   
+                            title: "Error",   
+                            text: response.message,   
+                            icon: "error", 
+                            showCancelButton: false,
+                            showConfirmButton: false,
+                            timer: 1500
+                        });
+                        
+                        btnvalobj.text(btnval);
+                        btnvalobj.attr('disabled', false);
+                    }
+                },
+                error       : function(data){
+                    var errors = $.parseJSON(data.responseText);
+                    $.each(errors.errors, function(index, value) {
+                        $('[name="'+index+'"]').addClass('error is-invalid');
+                        if($('[name="'+index+'"]').length)
+                        {
+                            $('[name="'+index+'"]').after('<span class="text-danger errormessage">'+value+'</span>');
+                        }
+                        else
+                        {
+                            $('[name="'+index+'[]"]').after('<span class="text-danger errormessage">'+value+'</span>');
+                        }
                     });
-                     $(".savebtn").html(btnval);
-                     $(".savebtn").attr('disabled', false);
-                 }
-             },
-             error       : function(data){
-                 var errors = $.parseJSON(data.responseText);
-                 $.each(errors.errors, function(index, value) {
-                     $('[name="'+index+'"]').addClass('error is-invalid');
-                     if($('[name="'+index+'"]').length)
-                     {
-                         $('[name="'+index+'"]').after('<span class="text-danger errormessage">'+value+'</span>');
-                     }
-                     else
-                     {
-                        $('[name="'+index+'[]"]').after('<span class="text-danger errormessage">'+value+'</span>');
-                     }
-                 });
-                 $(".savebtn").html(btnval);
-                 $(".savebtn").attr('disabled', false);
-             }
- 
-      });
-         return false;
-     });
+                    
+                    btnvalobj.text(btnval);
+                    btnvalobj.attr('disabled', false);
+                }
+
+        });
+            return false;
+        });
     }
 });
 
